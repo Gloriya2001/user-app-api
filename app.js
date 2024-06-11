6a2d29238147b99ca71ae2f3790d796501bff3c8
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-11 23:19:04 +0530

    imported the model

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const {usermodel} = require("./models/user")
+
 
 const app = express()
 app.use(cors())
